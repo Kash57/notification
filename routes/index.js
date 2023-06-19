86a0c931d7b396799a0ef24117c0c02651064b7c
Kashish Soni <kashish.soni@adintors.com>
2023-06-19 17:21:14 +0530

    another13 updated

--- routes/index.js
+++ routes/index.js
@@ -235,69 +235,164 @@ const DeviceDetector = require('device-detector-js');
 
 
 
-const UAParser = require('ua-parser-js');
+// const UAParser = require('ua-parser-js');
+// const dns = require('dns');
+
+// // Function to perform reverse DNS lookup
+// async function getHostName(ipAddress) {
+//   return new Promise((resolve, reject) => {
+//     // Exclude loopback address for reverse DNS lookup
+//     if (ipAddress === '::1' || ipAddress === '::ffff:127.0.0.1') {
+     
+//     } else {
+//       dns.reverse(ipAddress, (err, hostnames) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           const hostname = hostnames.length > 0 ? hostnames[0] : 'Unknown Hostname';
+//           resolve(hostname);
+//         }
+//       });
+//     }
+//   });
+// }
+
+// // API endpoint for handling login
+// router.get('/login', async (req, res) => {
+//   // Extract user agent from request headers
+//   const userAgent = req.headers['user-agent'];
+
+//   // Extract IP address of the user
+//   const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+//   try {
+//     // Parse user agent
+//     const parser = new UAParser();
+//     const uaResult = parser.setUA(userAgent).getResult();
+
+//     // Extract device information
+//     const deviceName = uaResult.device.model || 'Unknown Device';
+
+//     // Perform reverse DNS lookup for the provided IP address
+//     const hostName = await getHostName(ipAddress);
+
+//     // Construct device info object
+//     const deviceInfo = {
+//       device: deviceName,
+//       operatingSystem: uaResult.os.name,
+//       browser: uaResult.browser.name,
+//       hostName: hostName,
+//     };
+
+//     // Store the device information in your database
+//     // ...
+
+//     console.log(deviceInfo);
+//     res.json({ deviceInfo });
+//     // Rest of your code
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
+
+// // Function to fetch user IP address
+// async function getUserIPAddress() {
+//   const response = await fetch('https://api.ipify.org?format=json');
+//   const data = await response.json();
+//   return data.ip;
+// }
+
+// // Function to fetch user device details
+// function getUserDeviceDetails() {
+//   const userAgent = navigator.userAgent;
+//   const deviceName = navigator.deviceMemory || 'Unknown Device';
+//   const deviceLocation = navigator.language || 'Unknown Location';
+  
+//   return {
+//     deviceName,
+//     deviceLocation,
+//     userAgent
+//   };
+// }
 const dns = require('dns');
+// Function to perform the login action
+// Function to fetch user IP address
+async function getUserIPAddress() {
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  return data.ip;
+}
+
+// Function to fetch user device details
+function getUserDeviceDetails() {
+  const userAgent = navigator.userAgent;
+  const deviceName = navigator.deviceMemory || 'Unknown Device';
+  const deviceLocation = navigator.language || 'Unknown Location';
+  
+  return {
+    deviceName,
+    deviceLocation,
+    userAgent
+  };
+}
+
+// Function to perform the login action
+async function login() {
+  const ipAddress = await getUserIPAddress();
+  const deviceDetails = getUserDeviceDetails();
+
+  const body = {
+    ipAddress,
+    ...deviceDetails
+  };
+
+  const response = await fetch('/login', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+  console.log('Server response:', data);
+}
 
 // Function to perform reverse DNS lookup
 async function getHostName(ipAddress) {
   return new Promise((resolve, reject) => {
-    // Exclude loopback address for reverse DNS lookup
-    if (ipAddress === '::1' || ipAddress === '::ffff:127.0.0.1') {
-      resolve('Localhost');
-    } else {
-      dns.reverse(ipAddress, (err, hostnames) => {
-        if (err) {
-          reject(err);
-        } else {
-          const hostname = hostnames.length > 0 ? hostnames[0] : 'Unknown Hostname';
-          resolve(hostname);
-        }
-      });
-    }
+    dns.reverse(ipAddress, (err, hostnames) => {
+      if (err) {
+        reject(err);
+      } else {
+        const hostname = hostnames.length > 0 ? hostnames[0] : 'Unknown Hostname';
+        resolve(hostname);
+      }
+    });
   });
 }
 
-// API endpoint for handling login
-router.get('/login', async (req, res) => {
-  // Extract user agent from request headers
-  const userAgent = req.headers['user-agent'];
+// /login route
+router.get('/login', (req, res) => {
+  const { ipAddress, deviceName, deviceLocation, userAgent } = req.body;
+  const hostName = req.hostname || 'Unknown Hostname';
 
-  // Extract IP address of the user
-  const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  // Construct the user info object
+  const userInfo = {
+    ipAddress,
+    deviceName,
+    deviceLocation,
+    userAgent,
+    hostName
+  };
 
-  try {
-    // Parse user agent
-    const parser = new UAParser();
-    const uaResult = parser.setUA(userAgent).getResult();
+  // Store the user info in your database or perform other operations
+  // ...
 
-    // Extract device information
-    const deviceName = uaResult.device.model || 'Unknown Device';
-
-    // Perform reverse DNS lookup for the provided IP address
-    const hostName = await getHostName(ipAddress);
-
-    // Construct device info object
-    const deviceInfo = {
-      device: deviceName,
-      operatingSystem: uaResult.os.name,
-      browser: uaResult.browser.name,
-      hostName: hostName,
-    };
-
-    // Store the device information in your database
-    // ...
-
-    console.log(deviceInfo);
-    res.json({ deviceInfo });
-    // Rest of your code
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+  console.log('User Info:', userInfo);
+  res.json({ message: 'User info received successfully' });
 });
-
-
-
 
 
 
